e8fd35fd4ccc46905ddb860b48957062f6120d49
shun-harutaro <shun_harutaro@protonmail.com>
2022-06-15 14:36:19 +0900

    implement a for-loop that exert func

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 /* exert file */
 'use strict'
 const APP = require('./app');
-const POST = require('./post')
+const POST = require('./post');
 const OBJ = APP.read();
 //const CREATE = POST.createEvent();
 
-//console.log({OBJ});
-const eight = POST.createEvent(8, OBJ)
-
-//console.log(eight);
-
+const items = Object.keys(OBJ).length;
+for (var i = 0; i < items; i++) {
+    POST.createEvent(i + 1, OBJ);
+}
