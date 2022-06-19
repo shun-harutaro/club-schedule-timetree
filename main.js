d5106032cf8bf0adeaf2c7c26e36f9ef362e3120
shun-harutaro <shun_harutaro@protonmail.com>
2022-06-19 12:35:18 +0900

    Other problems found

--- main.js
+++ main.js
@@ -1,13 +1,57 @@
 /* exert file */
 'use strict'
+/**
+ * Responds to any HTTP request.
+ * @param {!express:request} req HTTP request context.
+ * @param {!express:response} res HTTP response context.
+ */
+ const axiosBase = require('axios');
+ require('dotenv').config();
+ 
+ // 環境変数(.env.yaml)
+ const TIMETREE_PERSONAL_TOKEN = process.env.timetreetoken; // パーソナルアクセストークン
+ const TIMETREE_CALENDAR_ID = process.env.timetreeid; // calendarid
+ 
+ const timetree = axiosBase.create({
+     baseURL: 'https://timetreeapis.com/', // クライアント
+     headers: {
+       'Content-Type': 'application/json', // データ形式
+       'Accept': 'application/vnd.timetree.v1+json', //APIバージョン
+       'Authorization': `Bearer ${TIMETREE_PERSONAL_TOKEN}` // パーソナルアクセストークンによる認証
+     },
+     responseType: 'json'
+ });
+
 const APP = require('./app');
 const POST = require('./post');
 const OBJ = APP.read();
 const items = Object.keys(OBJ).length;
-for (var i = 0; i < items; i++) {
-    const obj = OBJ[i + 1];
-    if (obj.activity !== true) {
-        continue;
+
+const postAll = async () => {
+    for (var i = 0; i < items; i++) {
+        console.log(`schedule of ${i+1}`);
+        const obj = OBJ[i + 1];
+        if (obj.activity !== true) {
+            console.log(`There aren't activity`);
+            continue;
+        }
+        const params = POST.jsonSet(obj);
+        await timetree.post(`calendars/${TIMETREE_CALENDAR_ID}/events`, JSON.stringify(params))
+        .then(res => {
+            console.log("success");
+        })
+        .catch(err => {
+            console.log("faild");
+            const code = err.response.data.errors;
+            console.log({code});
+        });
     }
-    const res = POST.createEvent(obj);
+    console.log(`${items} items are done`);
 }
+postAll();
+
+/*
+const obj = OBJ[20];
+const params = POST.jsonSet(obj);
+timetree.post(`calendars/${TIMETREE_CALENDAR_ID}/events`, JSON.stringify(params));
+*/
--- post.js
+++ post.js
@@ -1,27 +1,6 @@
-/* This file for API */
+/* This file make request */
 'use strict';
-/**
- * Responds to any HTTP request.
- * @param {!express:request} req HTTP request context.
- * @param {!express:response} res HTTP response context.
- */
-const axiosBase = require('axios');
-require('dotenv').config();
-
-// 環境変数(.env.yaml)
-const TIMETREE_PERSONAL_TOKEN = process.env.timetreetoken; // パーソナルアクセストークン
-const TIMETREE_CALENDAR_ID = process.env.timetreeid; // calendarid
-
-const timetree = axiosBase.create({
-    baseURL: 'https://timetreeapis.com/', // クライアント
-    headers: {
-      'Content-Type': 'application/json', // データ形式
-      'Accept': 'application/vnd.timetree.v1+json', //APIバージョン
-      'Authorization': `Bearer ${TIMETREE_PERSONAL_TOKEN}` // パーソナルアクセストークンによる認証
-    },
-    responseType: 'json'
-});
-
+const TIMETREE_CALENDAR_ID = process.env.timetreeid;
 // POST /calendars/:calendar_id/events のときのパラメーター
 // https://developers.timetreeapp.com/ja/docs/api#post-calendarscalendar_idevents
 const params = {
@@ -40,7 +19,7 @@ const params = {
         relationships: {
             label: {
                 data: {
-                    id: `${TIMETREE_CALENDAR_ID},1`, // ラベル（未提出:#e73b3b）
+                    id: `${TIMETREE_CALENDAR_ID},3`, // ラベル（未提出:#e73b3b）
                     type: "label"
                 }
             }
@@ -66,7 +45,8 @@ const dateMake = (date, startMs, endMs) => {
     return [isoStrStart, isoStrEnd];
 }
 
-const jsonSet = (obj) => {
+exports.jsonSet = (obj) => {
+    
     let atr = params.data.attributes;
     const [startMs, endMs] = divideTimeMs(obj.time)
     if (isNaN(startMs + endMs)) {
@@ -83,16 +63,21 @@ const jsonSet = (obj) => {
     atr.description = `- 担当教員：${obj.teacher}
 - 活動可能場所：${obj.detail}
 - 備考：${obj.remark}`;
+    return params;
 }
 
+/*
 exports.createEvent = (obj) => {
     jsonSet(obj);
     timetree.post(`calendars/${TIMETREE_CALENDAR_ID}/events`, JSON.stringify(params))
         .then(res => {
-            console.log(res)
+            console.log("success");
+            return res;
         })
         .catch(err => {
-            console.log(err)
+            console.log("faild");
+            console.log({err});
+            return err.response;
         });
-    return 0;
 }
+*/
